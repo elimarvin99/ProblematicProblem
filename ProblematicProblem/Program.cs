d713e6f0f9198ae48bba4616839e1fdaa42e7fb4
elimarvin99 <elimarvin99@gmail.com>
2021-11-16 11:40:11 -0500

    added if/else to exit program if player doesn't want to generate a random activity

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -15,57 +15,31 @@ namespace ProblematicProblem
         {
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
             var input = Console.ReadLine().ToLower();
-            //if (input.ToLower() == "yes")
-            //{
-
-            //}
-
-            Console.WriteLine();
-
-            Console.Write("We are going to need your information first! What is your name? ");
-            string userName = Console.ReadLine();
-
-            Console.WriteLine();
-
-            Console.Write("What is your age? ");
-            int userAge = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            var answer = Console.ReadLine();
-            bool seeList = false;
-            if (answer.ToLower() == "sure")
+            if (input.ToLower() == "yes")
             {
-                seeList = true;
-            }
-            
-
-            if (seeList)
-            {
-                foreach (string activity in activities)
-                {
-                    Console.Write($"{activity} ");
-                    Thread.Sleep(250);
-                }
-
-                Console.WriteLine();
-                Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = false;
-                var answer1 = Console.ReadLine();
-                if (answer1.ToLower() == "yes")
-                {
-                    addToList = true;
-                }
                 Console.WriteLine();
 
-                while (addToList)
+                Console.Write("We are going to need your information first! What is your name? ");
+                string userName = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.Write("What is your age? ");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+                var answer = Console.ReadLine();
+                bool seeList = false;
+                if (answer.ToLower() == "sure")
                 {
-                    Console.Write("What would you like to add? ");
-                    string userAddition = Console.ReadLine();
+                    seeList = true;
+                }
 
-                    activities.Add(userAddition);
 
+                if (seeList)
+                {
                     foreach (string activity in activities)
                     {
                         Console.Write($"{activity} ");
@@ -73,71 +47,101 @@ namespace ProblematicProblem
                     }
 
                     Console.WriteLine();
-                    Console.WriteLine("Would you like to add more? yes/no: ");
-                    var answer2 = Console.ReadLine();
-                    if (answer2.ToLower() == "yes")
+                    Console.Write("Would you like to add any activities before we generate one? yes/no: ");
+                    bool addToList = false;
+                    var answer1 = Console.ReadLine();
+                    if (answer1.ToLower() == "yes")
                     {
                         addToList = true;
                     }
+                    Console.WriteLine();
+
+                    while (addToList)
+                    {
+                        Console.Write("What would you like to add? ");
+                        string userAddition = Console.ReadLine();
+
+                        activities.Add(userAddition);
+
+                        foreach (string activity in activities)
+                        {
+                            Console.Write($"{activity} ");
+                            Thread.Sleep(250);
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Would you like to add more? yes/no: ");
+                        var answer2 = Console.ReadLine();
+                        if (answer2.ToLower() == "yes")
+                        {
+                            addToList = true;
+                        }
+                        else
+                        {
+                            addToList = false;
+                        }
+
+                    }
+                }
+
+                while (cont)
+                {
+                    Console.Write("Connecting to the database");
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.Write(". ");
+                        Thread.Sleep(500);
+                    }
+
+                    Console.WriteLine();
+
+                    Console.Write("Choosing your random activity");
+
+                    for (int i = 0; i < 9; i++)
+                    {
+                        Console.Write(". ");
+                        Thread.Sleep(500);
+                    }
+
+                    Console.WriteLine();
+                    Random rng = new Random();          //have to instanciate an object of the random class to use it
+                    int randomNumber = rng.Next(activities.Count);
+
+                    string randomActivity = activities[randomNumber];
+
+                    if (userAge > 21 && randomActivity == "Wine Tasting")
+                    {
+                        Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
+                        Console.WriteLine("Pick something else!");
+
+                        activities.Remove(randomActivity);
+
+                        randomNumber = rng.Next(activities.Count);
+
+                        randomActivity = activities[randomNumber];
+                    }
+
+                    Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? yes/no: ");
+                    Console.WriteLine();
+                    var answer3 = Console.ReadLine();
+                    if (answer3.ToLower() == "yes")
+                    {
+                        cont = true;
+                    }
                     else
                     {
-                        addToList = false;
+                        cont = false;
                     }
-                   
+
                 }
             }
-            
-            while (cont)
+            else
             {
-                Console.Write("Connecting to the database");
-
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.Write(". ");
-                    Thread.Sleep(500);
-                }
-
-                Console.WriteLine();
-
-                Console.Write("Choosing your random activity");
-
-                for (int i = 0; i < 9; i++)
-                {
-                    Console.Write(". ");
-                    Thread.Sleep(500);
-                }
-
-                Console.WriteLine();
-                Random rng = new Random();          //have to instanciate an object of the random class to use it
-                int randomNumber = rng.Next(activities.Count);
-
-                string randomActivity = activities[randomNumber];
-
-                if (userAge > 21 && randomActivity == "Wine Tasting")
-                {
-                    Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
-                    Console.WriteLine("Pick something else!");
-
-                    activities.Remove(randomActivity);
-
-                    randomNumber = rng.Next(activities.Count);
-
-                    randomActivity = activities[randomNumber];
-                }
-
-                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? yes/no: ");
-                Console.WriteLine();
-                var answer3 = Console.ReadLine();
-                if (answer3.ToLower() == "yes")
-                {
-                    cont = true;
-                }
-                else
-                {
-                    cont = false;
-                }
-
+                Console.WriteLine("Have a great day");
             }
         }
+
+           
     }
 }
